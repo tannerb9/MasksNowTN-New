c0eeaad0b77545465bfe9c10ccb3a52ad8590d44
tannerb9 <tanner-brainard@hotmail.com>
2021-02-19 01:38:21 -0500

    feat: added modal to display materials needed on Donate Materials card

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -37,6 +37,31 @@
           All donations stay in TN and are used to make masks for
           Tennessean's who need them!</v-card-text
         >
+        <v-dialog v-model="dialog">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on">
+              click here to see materials needed
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="headline">
+              MATERIALS NEEDED
+            </v-card-title>
+            <ul>
+              <li v-for="material in materials" :key="material.text">
+                {{ material.text }}
+              </li>
+            </ul>
+            <v-card-text>
+              Contact:
+              <span
+                ><a href="mailto:distribution.make@gmail.com"
+                  >distribution.make@gmail.com</a
+                ></span
+              ></v-card-text
+            >
+          </v-card>
+        </v-dialog>
         <v-card-actions>
           <v-btn outlined rounded text>
             <a
@@ -67,6 +92,37 @@
 <script>
 export default {
   name: "Help",
+  data() {
+    return {
+      materials: [
+        { text: "50% or higher cotton fabric (for the actual mask)" },
+        {
+          text:
+            "Thin elastic (¼-¾” ideally) to hold the mask onto the head",
+        },
+        {
+          text:
+            "T-shirt or similarly stretchy material (this can be cut into ties and used in place of elastic in a pinch)",
+        },
+        {
+          text:
+            "Nosepieces or thin pliable wire that won't poke through masks (even paper clips, and )",
+        },
+        { text: "Interfacing" },
+        { text: "Bias tape" },
+        { text: "90mm and 45mm Blades for rotary cutters" },
+        {
+          text:
+            "Bags with a sealing feature (like Ziploc) in which finished masks are sent out in",
+        },
+        { text: "Packaging tape" },
+        {
+          text:
+            "Cutting mats (ours are worn through from cutting so many kits)",
+        },
+      ],
+    };
+  },
   methods: {
     goToNationalSite: function() {
       window.open("https://masksnow.org", "_blank");
