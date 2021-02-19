abd7a67a182867432259b0ab43b34e8e05ff4d16
tannerb9 <tanner-brainard@hotmail.com>
2021-02-18 23:56:16 -0500

    feat: created basic structure for Help component but no styling

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,5 +1,64 @@
 <template>
-  <h1>Help Bar</h1>
+  <v-row justify="space-around">
+    <v-col cols="auto">
+      <v-card>
+        <v-card-title> Volunteer </v-card-title>
+        <v-card-text>
+          There are many ways to help, including sewing, delivering and
+          making kits</v-card-text
+        >
+        <v-card-actions>
+          <v-btn outlined rounded text>
+            Button
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="auto">
+      <v-card>
+        <v-card-title> Donate Funds </v-card-title>
+        <v-card-text>
+          All monetary donations stay in TN and go towards purchasing
+          mask-making materials and kits (fabric, elastic, rotary blade
+          replacements, cutting mats, ink, paper, etc.) and mailing
+          masks and kits to those who need them!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn outlined rounded text>
+            Button
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="auto">
+      <v-card>
+        <v-card-title> Donate Materials </v-card-title>
+        <v-card-text>
+          All donations stay in TN and are used to make masks for
+          Tennessean's who need them!</v-card-text
+        >
+        <v-card-actions>
+          <v-btn outlined rounded text>
+            Button
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="auto">
+      <v-card>
+        <v-card-title> National </v-card-title>
+        <v-card-text>
+          We are the Tennessee chapter of MaskNow.org. Not from TN? Find
+          your state’s chapter.</v-card-text
+        >
+        <v-card-actions>
+          <v-btn outlined rounded text>
+            Button
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
