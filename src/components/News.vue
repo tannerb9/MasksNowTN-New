7cb8438a1875e1e0f58beef5dc7a6af29650aa29
tannerb9 <tanner-brainard@hotmail.com>
2021-02-20 23:14:35 -0500

    feat: updated sample carousel to target object from script's data function and populate with correct data. needs to be styled

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,9 +1,14 @@
 <template>
   <v-carousel v-model="model">
-    <v-carousel-item v-for="(color, i) in newsPieces" :key="color">
-      <v-sheet :color="color" height="100%" tile>
+    <v-carousel-item v-for="(piece, i) in newsPieces" :key="colors[i]">
+      <v-sheet :color="colors[i]" height="100%" tile>
         <v-row class="fill-height" align="center" justify="center">
-          <div class="display-3">Slide {{ i + 1 }}</div>
+          <div class="display-3">{{ newsPieces[i].title }}</div>
+          <v-img v-if="newsPieces[i].image" :src="newsPieces[i].image">
+          </v-img>
+          <div class="display-3" v-if="newsPieces[i].textBody">
+            {{ newsPieces[i].textBody }}
+          </div>
         </v-row>
       </v-sheet>
     </v-carousel-item>
@@ -13,37 +18,38 @@
 <script>
 export default {
   name: "News",
-  data: () => ({
-    model: 0,
-    newsPieces: [
-      {
-        title: "Ben Beard Donates 10,000 Masks",
-        image: require("@/img/MasksNowTNLogo.png"),
-        textBody: null,
-        action: function() {},
-      },
-      {
-        title: "Chef's Market Donation",
-        image: null,
-        textBody:
-          "Chef's Market of Goodlettsville donated three full garbage bags of fabric. These were going to be used at a quilting program for incarcerated individuals, but that program was postponed.",
-        action: null,
-      },
-      {
-        title: "Thomas Brent and Evelyn Graham",
-        image: null,
-        textBody:
-          "Thomas Brent and Evelyn Graham of Nashville ordered 107 masks for his medical practice. They took so long to arrive that he had to purchase others. He donated the original order to Masks NOW.",
-        action: null,
-      },
-    ],
-    colors: [
-      "primary",
-      "secondary",
-      "yellow darken-2",
-      "red",
-      "orange",
-    ],
-  }),
+  data() {
+    return {
+      newsPieces: [
+        {
+          title: "Ben Beard Donates 10,000 Masks",
+          image: require("@/img/BenBeard.png"),
+          textBody: null,
+          action: function() {},
+        },
+        {
+          title: "Chef's Market Donation",
+          image: null,
+          textBody:
+            "Chef's Market of Goodlettsville donated three full garbage bags of fabric. These were going to be used at a quilting program for incarcerated individuals, but that program was postponed.",
+          action: null,
+        },
+        {
+          title: "Thomas Brent and Evelyn Graham",
+          image: null,
+          textBody:
+            "Thomas Brent and Evelyn Graham of Nashville ordered 107 masks for his medical practice. They took so long to arrive that he had to purchase others. He donated the original order to Masks NOW.",
+          action: null,
+        },
+      ],
+      colors: [
+        "primary",
+        "secondary",
+        "yellow darken-2",
+        "red",
+        "orange",
+      ],
+    };
+  },
 };
 </script>
