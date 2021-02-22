35bb90f69ecf7ad1efdae1053bccf2599ca12e95
tannerb9 <tanner-brainard@hotmail.com>
2021-02-21 20:29:13 -0500

    feat: added v-footer to App.vue to use as Contact section

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,46 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+
+    <v-footer padless>
+      <v-card class="flex" flat tile>
+        <v-card-text class="py-0 black--text text-center">
+          <strong>Masks Now TN</strong>
+        </v-card-text>
+        <v-card-title>
+          <!-- <strong class="subheading"
+            >Get connected with us on social networks!</strong
+          >
+
+          <v-spacer></v-spacer> -->
+
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 black"
+            dark
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-title>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
+  data: () => ({
+    icons: ["mdi-email", "mdi-facebook", "mdi-instagram"],
+    socialMediaLinks: [
+      "https://www.facebook.com/groups/masksnowtn/",
+      "https://www.instagram.com/masksnowtn/",
+    ],
+  }),
 };
 </script>
 
