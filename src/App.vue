e80053b58d180c81623a97f6722a6ebf54e636aa
tannerb9 <tanner-brainard@hotmail.com>
2021-02-21 22:24:54 -0500

    feat: added FB, IG, and email icons to Contact(footer). all functioning properly

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,16 @@
           <strong>Masks Now TN</strong>
         </v-card-text>
         <v-card-title>
-          <!-- <strong class="subheading"
-            >Get connected with us on social networks!</strong
-          >
-
-          <v-spacer></v-spacer> -->
-
           <v-btn
             v-for="icon in icons"
             :key="icon"
             class="mx-4 black"
             dark
             icon
+            v-on:click="clickHandler(icon)"
           >
             <v-icon size="24px">
-              {{ icon }}
+              {{ icon.title }}
             </v-icon>
           </v-btn>
         </v-card-title>
@@ -42,11 +37,32 @@ export default {
   name: "App",
   data: () => ({
     icons: ["mdi-email", "mdi-facebook", "mdi-instagram"],
-    socialMediaLinks: [
-      "https://www.facebook.com/groups/masksnowtn/",
-      "https://www.instagram.com/masksnowtn/",
-    ],
   }),
+  methods: {
+    clickHandler: (str) => {
+      {
+        str == "mdi-email"
+          ? (window.location = "mailto:distribution.make@gmail.com")
+          : null;
+      }
+      {
+        str == "mdi-facebook"
+          ? window.open(
+              "https://www.facebook.com/groups/masksnowtn/",
+              "_blank"
+            )
+          : null;
+      }
+      {
+        str == "mdi-instagram"
+          ? window.open(
+              "https://www.instagram.com/masksnowtn/",
+              "_blank"
+            )
+          : null;
+      }
+    },
+  },
 };
 </script>
 
