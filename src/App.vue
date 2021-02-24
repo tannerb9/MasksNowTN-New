3a3e980639ebd13d35742128688ceabc50313df4
tannerb9 <tanner-brainard@hotmail.com>
2021-02-24 16:14:59 -0500

    refactor: made v-footer in App.vue into component Contact.vue and imported it into App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,61 +8,15 @@
       <router-view></router-view>
     </v-main>
 
-    <v-footer padless>
-      <v-card class="flex" flat tile>
-        <v-card-text class="py-0 black--text text-center">
-          <strong>Masks Now TN</strong>
-        </v-card-text>
-        <v-card-title>
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4 black"
-            dark
-            icon
-            v-on:click="clickHandler(icon)"
-          >
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-title>
-      </v-card>
-    </v-footer>
+    <Contact />
   </v-app>
 </template>
 
 <script>
+import Contact from "@/components/Contact";
 export default {
   name: "App",
-  data: () => ({
-    icons: ["mdi-email", "mdi-facebook", "mdi-instagram"],
-  }),
-  methods: {
-    clickHandler: (str) => {
-      {
-        str == "mdi-email"
-          ? (window.location = "mailto:distribution.make@gmail.com")
-          : null;
-      }
-      {
-        str == "mdi-facebook"
-          ? window.open(
-              "https://www.facebook.com/groups/masksnowtn/",
-              "_blank"
-            )
-          : null;
-      }
-      {
-        str == "mdi-instagram"
-          ? window.open(
-              "https://www.instagram.com/masksnowtn/",
-              "_blank"
-            )
-          : null;
-      }
-    },
-  },
+  components: { Contact },
 };
 </script>
 
---
+++ src/components/Contact.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-footer padless>
+    <v-card class="flex" flat tile>
+      <v-card-text class="py-0 black--text text-center">
+        <strong>Masks Now TN</strong>
+      </v-card-text>
+      <v-card-title>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 black"
+          dark
+          icon
+          v-on:click="clickHandler(icon)"
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-title>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+  data: () => ({
+    icons: ["mdi-email", "mdi-facebook", "mdi-instagram"],
+  }),
+  methods: {
+    clickHandler: (str) => {
+      {
+        str == "mdi-email"
+          ? (window.location = "mailto:distribution.make@gmail.com")
+          : null;
+      }
+      {
+        str == "mdi-facebook"
+          ? window.open(
+              "https://www.facebook.com/groups/masksnowtn/",
+              "_blank"
+            )
+          : null;
+      }
+      {
+        str == "mdi-instagram"
+          ? window.open(
+              "https://www.instagram.com/masksnowtn/",
+              "_blank"
+            )
+          : null;
+      }
+    },
+  },
+};
+</script>
