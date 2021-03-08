94bf031e8f73545ec96cb1860ac6e4526b75b6e9
Connor Blakeney <connorblakeney@yahoo.com>
2021-03-08 12:05:18 -0600

    trying to figure out bug where drawer opens after resizing and why first link is always underlined

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <v-app-bar app dense color="#153570" >
-              <v-btn @click="drawer = !drawer" class="hidden-md-and-up">
+              <v-btn color="#DEC328" @click="drawer = !drawer" class="hidden-md-and-up">
                 <v-icon>mdi-format-align-justify</v-icon>
               </v-btn>
             <template v-slot:extension>
@@ -16,33 +16,27 @@
         </v-app-bar>
 
         <v-navigation-drawer app v-model="drawer" class="hidden-md-and-up" >
-            <v-list
-                nav
-                dense
-            >
-                <v-list-item-group
-                v-model="group"
-                active-class="deep-purple--text text--accent-4"
-                >
-                <v-list-item>
-                    <v-list-item-title><a href="https://masksnow.org/">National Website</a></v-list-item-title>
-                </v-list-item>
+            <v-list nav dense>
+                <v-list-item-group v-model="group">
+                    <v-list-item>
+                        <v-list-item-title><a href="https://masksnow.org/">National Website</a></v-list-item-title>
+                    </v-list-item>
 
-                <v-list-item>
-                    <v-list-item-title><a href="https://charity.gofundme.com/o/en/campaign/masksnow-tennessee">Donate Funds</a></v-list-item-title>
-                </v-list-item>
+                    <v-list-item>
+                        <v-list-item-title><a href="https://charity.gofundme.com/o/en/campaign/masksnow-tennessee">Donate Funds</a></v-list-item-title>
+                    </v-list-item>
 
-                <v-list-item>
-                    <v-list-item-title><a href="mailto: distribution.make@gmail.com">Donate Materials</a></v-list-item-title>
-                </v-list-item>
+                    <v-list-item>
+                        <v-list-item-title><a href="mailto: distribution.make@gmail.com">Donate Materials</a></v-list-item-title>
+                    </v-list-item>
 
-                <v-list-item>
-                    <v-list-item-title><a href="https://masksnow.org/volunteer/">Volunteer</a></v-list-item-title>
-                </v-list-item>
+                    <v-list-item>
+                        <v-list-item-title><a href="https://masksnow.org/volunteer/">Volunteer</a></v-list-item-title>
+                    </v-list-item>
 
-                <v-list-item>
-                    <v-list-item-title><a href="https://airtable.com/embed/shrG3kgFmeLYyoxxn" target="airtable">Get Masks</a></v-list-item-title>
-                </v-list-item>
+                    <v-list-item>
+                        <v-list-item-title><a href="https://airtable.com/embed/shrG3kgFmeLYyoxxn" target="airtable">Get Masks</a></v-list-item-title>
+                    </v-list-item>
                 </v-list-item-group>
             </v-list>
         </v-navigation-drawer>
@@ -66,12 +60,12 @@ export default {
 </script>
 
 <style>
-
 .v-tabs {
     display: flex;
     justify-content: space-evenly;
     padding: 0 0 12vh 0;
 }
+
 .v-tab {
     background: #DEC328;
     border-radius: 50px;
@@ -83,17 +77,18 @@ export default {
     margin: 0.25rem;
 }
 
-.v-tab:hover:before, nav li:hover:after
-{
+.v-tab:hover:before, nav li:hover:after {
     transform: scale(1);
 }
-.v-app-bar a {
+
+a {
     text-decoration: none;
     overflow: hidden;
     white-space: nowrap;
     min-width: 1.8em;
     max-width: 100%;
 }
+
 .v-app-bar a:hover {
     transform: scale(1.07);
 }
