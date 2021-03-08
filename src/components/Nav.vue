9fd32b8e8863e16340031774a72790523aa45bce
Connor Blakeney <connorblakeney@yahoo.com>
2021-03-08 11:15:32 -0600

    navbar is responsive, drawer needs to be styled

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,75 +1,63 @@
 <template>
     <div id="app">
         <v-app-bar app dense color="#153570" >
-              <v-btn click="" class="hidden-sm-and-up">
+              <v-btn @click="drawer = !drawer" class="hidden-md-and-up">
                 <v-icon>mdi-format-align-justify</v-icon>
               </v-btn>
-              <v-toolbar-title class="hidden-sm-and-down">
-                <router-link to="/" tag="span" style="cursor: pointer">
-                <img
-                    src="@/assets/logo.png"
-                    alt="RTS ENVIRONMENTAL SERVICES, INC."
-                    class="mt-8 hidden-sm-and-down"
-                />
-                <img
-                    src="@/assets/logo.png"
-                    alt="RTS ENVIRONMENTAL SERVICES, INC."
-                    class="mt-1 hidden-md-and-up"
-                    height="50px"
-                />
-                </router-link>
-            </v-toolbar-title>
             <template v-slot:extension>
                 <v-tabs class="hidden-sm-and-down">
                     <v-tab><a href="https://masksnow.org/">National Website</a></v-tab>
                     <v-tab><a href="https://charity.gofundme.com/o/en/campaign/masksnow-tennessee">Donate Funds</a></v-tab>
-                    <v-tab><a href = "mailto: distribution.make@gmail.com">Donate Materials</a></v-tab>
+                    <v-tab><a href="mailto: distribution.make@gmail.com">Donate Materials</a></v-tab>
                     <v-tab><a href="https://masksnow.org/volunteer/">Volunteer</a></v-tab>
                     <v-tab><a href="https://airtable.com/embed/shrG3kgFmeLYyoxxn" target="airtable">Get Masks</a></v-tab>
                 </v-tabs>
             </template>
         </v-app-bar>
-    </div>
-<!-- <div id="app">
 
-  
-  <v-app-bar app dense color="#153570">
-  <span class="hidden-sm-and-up">
-    <v-app-bar-nav-icon @click="sidebar = !sidebar"></v-app-bar-nav-icon>
-  </span>
-  <v-toolbar-title>
-    <router-link to="/" tag="span" style="cursor: pointer">
-      <img
-        src="@/assets/logo.png"
-        alt="RTS ENVIRONMENTAL SERVICES, INC."
-        class="mt-8 hidden-sm-and-down"
-      />
-      <img
-        src="@/assets/logo.png"
-        alt="RTS ENVIRONMENTAL SERVICES, INC."
-        class="mt-1 hidden-md-and-up"
-        height="50px"
-      />
-    </router-link>
-  </v-toolbar-title>
-  <v-spacer></v-spacer>
-    <template v-slot:extension>
-                <v-tabs class="hidden-sm-and-down">
-                    <v-tab><a href="https://masksnow.org/">National Website</a></v-tab>
-                    <v-tab><a href="https://charity.gofundme.com/o/en/campaign/masksnow-tennessee">Donate Funds</a></v-tab>
-                    <v-tab><a href = "mailto: distribution.make@gmail.com">Donate Materials</a></v-tab>
-                    <v-tab><a href="https://masksnow.org/volunteer/">Volunteer</a></v-tab>
-                    <v-tab><a href="https://airtable.com/embed/shrG3kgFmeLYyoxxn" target="airtable">Get Masks</a></v-tab>
-                </v-tabs>
-            </template>
-</v-app-bar>
-</div> -->
+        <v-navigation-drawer app v-model="drawer" class="hidden-md-and-up" >
+            <v-list
+                nav
+                dense
+            >
+                <v-list-item-group
+                v-model="group"
+                active-class="deep-purple--text text--accent-4"
+                >
+                <v-list-item>
+                    <v-list-item-title><a href="https://masksnow.org/">National Website</a></v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-list-item-title><a href="https://charity.gofundme.com/o/en/campaign/masksnow-tennessee">Donate Funds</a></v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-list-item-title><a href="mailto: distribution.make@gmail.com">Donate Materials</a></v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-list-item-title><a href="https://masksnow.org/volunteer/">Volunteer</a></v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-list-item-title><a href="https://airtable.com/embed/shrG3kgFmeLYyoxxn" target="airtable">Get Masks</a></v-list-item-title>
+                </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
 </template>
 
 <script>
 export default {
   name: "Nav",
 
+  data() {
+    return {
+        drawer: false
+    }
+}
 
 };
 
